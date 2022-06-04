c820425990d012f3ca4025db01888e10be98e7e0
Danzka <bnandia34@yahoo.com>
2022-06-03 20:48:41 -0700

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ var scores = [0, 0];
 var roundScore = 0;
 //Шооны аль талаараа буусныг хадгалах хувьсагч, 1 - 6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
 
-//<div class="player-score" id="score-0">43</div>
-var dice = Math.floor(Math.random() * 6) + 1;
-// window.document.querySelector("#score-0").textContent = dice;
-// document.querySelector("#score-1").textContent = dice;
-
 //Програм эхлэхэд бэлтгэе
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
