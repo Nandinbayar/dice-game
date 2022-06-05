6c426e3521f8753d33c5f1cf217b7d60835d8319
Danzka <bnandia34@yahoo.com>
2022-06-04 17:53:18 -0700

    Toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-//Тоглогчийн ээлжийг хадгалах хувьсагч, Нэгдүгээр тоглогчийг 0, Хоёрдугаар тоглогчийг 1 гэж тэмджглэе
-var activePlayer = 1;
-//Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
+var activePlayer = 0;
 var scores = [0, 0];
-//Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
-//Шооны аль талаараа буусныг хадгалах хувьсагч, 1 - 6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
-
-//Програм эхлэхэд бэлтгэе
 document.getElementById("score-0").textContent = "0";
 document.getElementById("score-1").textContent = "0";
 document.getElementById("current-0").textContent = "0";
@@ -16,6 +10,20 @@ var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 document.querySelector(".btn-roll").addEventListener("click", function () {
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+
   diceDom.style.display = "block";
+
   diceDom.src = "dice-" + diceNumber + ".png";
+
+  if (diceNumber !== 1) {
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    diceDom.style.display = "none";
+  }
 });
